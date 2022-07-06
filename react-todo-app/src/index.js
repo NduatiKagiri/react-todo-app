02c99a661f05384d891432fa6759c43281380884
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-07-07 02:27:15 +0300

    Create todo-app

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
 //component file
 import TodoContainer from "./components/TodoContainer"
+
+//stylesheet
+import "./App.css"
+
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
